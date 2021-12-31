e221c7ae007b41f6b864cfa7675196a30b0fd37d
matthew1996i <matheus.pereira@lexartlabs.com>
2021-12-31 08:50:10 -0300

    add key from auth email

--- api/src/utils/email/sendEmail.js
+++ api/src/utils/email/sendEmail.js
@@ -1,32 +1,28 @@
-const mailjet = require('node-mailjet');
+const SibApiV3Sdk = require('sib-api-v3-sdk');
 
-module.exports = {
-  async sendEmail(body) {
-    const {
-      from,
-      to,
-      subject,
-      textPart,
-      emailHTML,
-    } = body;
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const { mailerToken } = require('../../config/auth.json');
 
-    await mailjet
-      .connect('09197ba65e51529f33255b33bba7f892', 'ddbba32133507c94d74aa7f1816219fb')
-      .post('send', { version: 'v3.1' })
-      .request({
-        Messages: [
-          {
-            From: {
-              Email: from.email,
-              Name: from.name,
-            },
-            To: to,
-            Subject: subject,
-            TextPart: textPart,
-            HTMLPart: emailHTML,
-            CustomID: 'AppGettingStartedTest',
-          },
-        ],
-      });
-  },
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = mailerToken;
+
+const apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
+const emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
+
+emailCampaigns.name = 'Campaign sent via the API';
+emailCampaigns.subject = 'My subject';
+emailCampaigns.sender = { name: 'From name', email: 'matthew199624@gmail.com' };
+emailCampaigns.type = 'classic';
+emailCampaigns.body = {
+  htmlContent: 'Congratulations! You successfully sent this example campaign via the Sendinblue API.',
+
+  recipients: { listIds: [2, 7] },
+
+  scheduledAt: '2018-01-01 00:00:01',
 };
+
+apiInstance.createEmailCampaign(emailCampaigns).then((data) => {
+  console.log(`API called successfully. Returned data: ${data}`);
+}, (error) => {
+  console.error(error);
+});
